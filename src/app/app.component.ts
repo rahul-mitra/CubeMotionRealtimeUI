4a55fe6b2fd5558f9c0ec55fec8eb822fb720856
rahul-mitra <rahul1991mira@gmail.com>
2021-05-15 12:06:10 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,162 @@
-import { Component } from '@angular/core';
+
+import { SocketService } from './../services/socket.service';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import * as THREE from 'three';
+import { isGeneratedFile } from '@angular/compiler/src/aot/util';
+import { MatSliderChange } from '@angular/material/slider';
+
+export class ColorCodesWTime {
+  colorCode: string;
+  time: Date
+  constructor(color: string) {
+    this.colorCode = color;
+    this.time = new Date;
+  }
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnDestroy, OnInit {
   title = 'cubeui';
+
+  public subs: Array<Subscription> = new Array<Subscription>();
+  camera!: THREE.PerspectiveCamera;
+  scene!: THREE.Scene;
+  renderer!: THREE.WebGLRenderer;
+  geometry!: THREE.BoxGeometry;
+  material!: THREE.MeshNormalMaterial;
+  mesh!: THREE.Mesh;
+  randomnum: number = 0;
+  maxValue: number = 5;
+  steps: number = 0.25;
+  colorsUsed: Array<ColorCodesWTime> = new Array<ColorCodesWTime>();
+  interaction: any;
+  cstX: number = 0;
+  cstY: number = 0;
+  cstZ: number = 0;
+  fpsElem!: Element;
+  fpsThen: number = 0;
+  constructor(private socketService: SocketService) {
+    this.threeInit()
+    this.subs.push(this.socketService.onServerMessages().subscribe(message => { console.log(message) }));
+    this.subs.push(this.socketService.onConnected().subscribe(message => { console.log(message) }));
+    this.subs.push(this.socketService.onEmitColor().subscribe(colorCode => {
+      this.changeColor(colorCode);
+      console.log("Recieved color code");
+    }));
+    this.subs.push(this.socketService.onEmitAxis().subscribe(axis => {
+      this.mesh.rotation.x = axis.x;
+      this.mesh.rotation.y = axis.y;
+      this.mesh.rotation.z = axis.z;
+      this.cstX = axis.x;
+      this.cstY = axis.y;
+      this.cstZ = axis.z;
+      this.setRenderer();
+    }));
+
+  }
+
+
+  ngOnInit(): void {
+    let canvas: HTMLElement = document.getElementsByTagName("canvas")[0];
+    canvas.addEventListener("click", () => { this.changeColor() });
+
+  }
+  ngOnDestroy(): void {
+    this.subs.forEach(sub => sub.unsubscribe());
+  }
+
+
+
+  changeX(x: MatSliderChange) {
+    if (x.value) {
+      this.mesh.rotation.x = x.value;
+      this.cstX = x.value;
+      this.socketService.emitAxis(this.cstX, this.cstY, this.cstZ);
+      this.setRenderer();
+    }
+  }
+  changeY(y: MatSliderChange) {
+
+    if (y.value) {
+      this.mesh.rotation.y = y.value;
+      this.cstY = y.value;
+      this.socketService.emitAxis(this.cstX, this.cstY, this.cstZ);
+      this.setRenderer();
+    }
+  }
+  changeZ(z: MatSliderChange) {
+
+    if (z.value) {
+      this.mesh.rotation.z = z.value;
+      this.cstZ = z.value;
+      this.socketService.emitAxis(this.cstX, this.cstY, this.cstZ);
+      this.setRenderer();
+    }
+  }
+
+  setRenderer() {
+    this.renderer.render(this.scene, this.camera)
+  }
+
+  public test() {
+    this.socketService.testServer();
+    // this.randomnum=this.randomnum+1;
+    // this.mesh.rotation.x = this.randomnum / 10;
+    // this.mesh.rotation.y = this.randomnum / 20;
+    // this.mesh.rotation.z = this.randomnum / 30;
+    // console.log(this.mesh.rotation.x);
+    // this.renderer.render(this.scene, this.camera);
+  }
+
+  public threeInit() {
+    this.camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 0.01, 10);
+    this.camera.position.z = 1;
+
+    this.scene = new THREE.Scene();
+
+    this.geometry = new THREE.BoxGeometry(0.2, 0.2, 0.2);
+    this.material = new THREE.MeshNormalMaterial();
+
+    this.mesh = new THREE.Mesh(this.geometry, this.material);
+    this.scene.add(this.mesh);
+    this.renderer = new THREE.WebGLRenderer({ antialias: true });
+    this.renderer.setSize(window.innerWidth, window.innerHeight);
+    // this.renderer.setAnimationLoop(this.animation);
+    this.renderer.render(this.scene, this.camera);
+    document.body.appendChild(this.renderer.domElement);
+  }
+  animation = (time: number) => {
+    if (this.mesh) {
+      this.mesh.rotation.x = time / 10000;
+      // this.mesh.rotation.y = time / 1000;
+      // this.mesh.rotation.z = time / 500;
+      this.renderer.render(this.scene, this.camera);
+      // console.log(this.mesh.rotation.x);
+    }
+  }
+
+  changeColor(colorCode?: string, callEmitter: boolean = false) {
+    if (colorCode) {
+      this.mesh.material = new THREE.MeshBasicMaterial({ color: colorCode });
+      this.colorsUsed.push(new ColorCodesWTime(colorCode));
+      if (callEmitter)
+        this.socketService.emitColor(colorCode);
+    }
+    else {
+      let color = this.randomColor();
+      this.socketService.emitColor(color);
+      this.mesh.material = new THREE.MeshBasicMaterial({ color: color })
+      this.colorsUsed.push(new ColorCodesWTime(color));
+    }
+    this.renderer.render(this.scene, this.camera);
+  }
+
+  randomColor() {
+    return "#" + Math.floor(Math.random() * 16777215).toString(16)
+  }
 }
---
+++ src/services/socket.service.ts
@@ -0,0 +1,81 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { io, Socket } from 'socket.io-client';
+
+// export const socketServerURL: string = "https://api.quickchat.rahulmitra.dev"
+// export const socketServerURL: string = "http://140.238.226.73:3000"
+export const socketServerURL: string = "http://localhost:3000";
+// export const socketServerURL: string = "https://rh-chat-server.herokuapp.com";
+const options = {
+  reconnectionDelayMax: 10000,
+  reconnectionAttempts: 5,
+  transports: ["websocket"],
+  upgrade: false,
+  reconnection: true,
+}
+@Injectable({
+  providedIn: 'root'
+})
+export class SocketService {
+  private socketClient!: Socket;
+  constructor() {
+    this.initSocket();
+  }
+  public  initSocket(){
+    this.socketClient =  io(socketServerURL, options);
+    console.log("socket initialized");
+  }
+  public onConnected(): Observable<string> {
+    var observer = new Observable<string>((observer) => {
+      this.socketClient.on("connect", () => {
+        observer.next("Connected to server")
+      });
+    });
+    return observer;
+  }
+  public disconnect() {
+    this.socketClient.disconnect();
+  }
+  public testServer() {
+    // console.log("Connection status ",this.socketClient.connected);
+    this.socketClient.emit("testServer", "testing 123");
+  }
+
+
+  public onServerMessages(): Observable<string> {
+    return new Observable<string>((observer) => {
+      this.socketClient.on("serverMessage", (message: string) => {
+        observer.next(message)
+      });
+    });
+  }
+
+  public emitAxis(x:number,y:number,z:number)
+  {
+    this.socketClient.emit("emitAxis",{x:x,y:y,z:z})
+  }
+
+  public onEmitAxis(): Observable<{x:number,y:number,z:number}> {
+    return new Observable<{x:number,y:number,z:number}>((observer) => {
+      this.socketClient.on("emitAxis", (axis:{x:number,y:number,z:number}) => {
+        observer.next(axis)
+      });
+    });
+  }
+
+  public emitColor(colorCode:string)
+  {
+    console.log("emitting color",colorCode);
+    this.socketClient.emit("emitColor",colorCode)
+  }
+
+  public onEmitColor(): Observable<string> {
+    return new Observable<string>((observer) => {
+      this.socketClient.on("emitColor", (colorCode:string) => {
+        observer.next(colorCode)
+      });
+    });
+  }
+
+
+}
